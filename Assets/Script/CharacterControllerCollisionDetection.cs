3a0cfc9339b3f9c5e95971be72baba780d1461b6
Samuel Rother <35152484+Sigma42@users.noreply.github.com>
2023-12-07 14:32:02 +0100

    replace the new's in every FixUpdate with Constants.

--- Assets/Script/CharacterControllerCollisionDetection.cs
+++ Assets/Script/CharacterControllerCollisionDetection.cs
@@ -4,6 +4,9 @@ public class CharacterControllerColissionDetection : MonoBehaviour
 {
     private CharacterController _characterController;
 
+    private readonly static Vector3 _small_move = new(0.001f, -0.001f, 0.001f);
+    private readonly static Vector3 _small_move_back = new(-0.001f, 0.001f, -0.001f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,7 @@ public class CharacterControllerColissionDetection : MonoBehaviour
     {
         // execute minimal movement on each physic update to trigger collision detection of the character controller
         // player will not notice the movement
-        _characterController.Move(new Vector3(0.001f, -0.001f, 0.001f));
-        _characterController.Move(new Vector3(-0.001f, 0.001f, -0.001f));
+        _characterController.Move(_small_move);
+        _characterController.Move(_small_move_back);
     }
 }
