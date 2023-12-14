bb111b8012f098c98b2c513c27663ff91957d387
Besteresis <vpetry6@gmail.com>
2023-12-14 21:44:21 +0100

    Code Convention applied

--- Assets/Editor/CollisionTriggeredTeleportEditor.cs
+++ Assets/Editor/CollisionTriggeredTeleportEditor.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
+// Changes the Inspector Editor of the CollisionTriggeredTeleport Class
 [CustomEditor(typeof(CollisionTriggeredTeleport))]
 public class CollisionTriggeredTeleportEditor : Editor
 {
@@ -16,8 +17,10 @@ public class CollisionTriggeredTeleportEditor : Editor
     
     SerializedProperty _collidingLayer;
 
+    // OnEnable is called when the GameObject is loaded
     private void OnEnable()
     {
+        // Find serialized Properties and store them
         _transformParent = serializedObject.FindProperty("_transformParent");
         _vectorType = serializedObject.FindProperty("_vectorType");
         _teleportVector = serializedObject.FindProperty("_teleportVector");
@@ -26,19 +29,24 @@ public class CollisionTriggeredTeleportEditor : Editor
         _collidingLayer = serializedObject.FindProperty("_collidingLayer");
     }
 
+    // OnInspectorGUI specifies the way the Inspector Editor should be drawn
     public override void OnInspectorGUI()
     {
+        // Get the current instance of CollisionTriggeredTeleport (target only exists in this context)
         CollisionTriggeredTeleport collisionTriggeredTeleport = (CollisionTriggeredTeleport)target;
 
         serializedObject.Update();
 
+        // Generate the non-editable script reference a normal script component has
         using (new EditorGUI.DisabledScope(true)) EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour((MonoBehaviour)target), GetType(), false);
 
+        // Generate the following fields in the Inspector Editor
         EditorGUILayout.PropertyField(_transformParent);
         EditorGUILayout.PropertyField(_vectorType);
         EditorGUILayout.PropertyField(_teleportVector);
         EditorGUILayout.PropertyField(detectUsing);
 
+        // Show either Tag or Layer depending on the choosen Detection type
         if (collisionTriggeredTeleport.detectUsing == CollisionTriggeredTeleport.DetectUsing.Tag)
         {
             EditorGUILayout.PropertyField(_collidingTag);
