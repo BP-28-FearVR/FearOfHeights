83508823cdb5b3d3720713e8831f3d91768a7af5
Samuel Rother <35152484+Sigma42@users.noreply.github.com>
2024-01-16 20:42:16 +0100

    Set the preset Value in Origin to Every/Nothing -> Use script to force include the layers

--- Assets/Script/CharacterControllerCollisionDetection.cs
+++ Assets/Script/CharacterControllerCollisionDetection.cs
@@ -7,11 +7,25 @@ public class CharacterControllerColissionDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Gets CharacterController
         _characterController = GetComponent<CharacterController>();
-    }
 
-    private void FixedUpdate()
-    {
+        
+        if ( _characterController != null)
+        {
+            //Set layerOverridePriority
+            if (_characterController.layerOverridePriority < 1)
+            {
+                _characterController.layerOverridePriority = 1;
+            }
 
+            LayerMask mustIncludeLayers = LayerMask.GetMask(new string[] { "Floor", "Trigger" });
+
+            //Bitwise logic to combine existing exclude/include
+            _characterController.includeLayers = _characterController.includeLayers | mustIncludeLayers;
+            _characterController.excludeLayers = _characterController.excludeLayers & (~mustIncludeLayers);
+        }
+        
+        
     }
 }
