0a76f1c75892c0e022003bd295c9bc741199a7c8
Samuel Rother <35152484+Sigma42@users.noreply.github.com>
2024-01-16 20:20:24 +0100

    Added Floor, Trigger Layers & Ignore collion for Player but Floor & Trigger & Change Layers where neccesary

--- Assets/Script/CharacterControllerCollisionDetection.cs
+++ Assets/Script/CharacterControllerCollisionDetection.cs
@@ -4,9 +4,6 @@ public class CharacterControllerColissionDetection : MonoBehaviour
 {
     private CharacterController _characterController;
 
-    private readonly static Vector3 _small_move = new(0.001f, -0.001f, 0.001f);
-    private readonly static Vector3 _small_move_back = new(-0.001f, 0.001f, -0.001f);
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +12,6 @@ public class CharacterControllerColissionDetection : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // execute minimal movement on each physic update to trigger collision detection of the character controller
-        // player will not notice the movement
-        _characterController.Move(_small_move);
-        _characterController.Move(_small_move_back);
+
     }
 }
