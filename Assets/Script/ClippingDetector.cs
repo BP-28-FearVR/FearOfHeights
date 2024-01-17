2efa0f45c8e6a23d44aac75ce155633c6b10ee0a
Besteresis <vpetry6@gmail.com>
2024-01-17 23:07:16 +0100

    ClippingDetector implementiert

--- Assets/Script/ClippingDetector.cs
+++ Assets/Script/ClippingDetector.cs
@@ -1,48 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
+// This class handles collision of it's GameObject with other Objects, fading the screen to Black if the other GameObject has the specified Layer
+// Both this GameObject need a Collider, while this GameObject needs the Collider to be a Trigger and the other to not be a Trigger.
+// This GameObject also need a RigidBody
 public class ClippingDetector : MonoBehaviour
 {
-    public Animator animator;
-    public string forbiddenLayerName;
+    private static string defaultLayerName = "Default";
 
-    private int amountClippedThrough = 0;
-    private int forbiddenLayerIndex;
+    [Tooltip("The Animator to use for animating the Black Screen")]
+    [SerializeField] private Animator animator;
 
-    private void Awake()
+    [Tooltip("The Name of the Layer that when near it the Screen should fade to black")]
+    [SerializeField] private string forbiddenLayerName = defaultLayerName;
+
+    // The amount of Objects with the specified Layer this GameObject is clipping through
+    private int _amountClippedThrough = 0;
+
+    // Layer comparison is done in int, calculation result of the layer conversion to int is saved
+    private int _forbiddenLayerIndex;
+
+    // Is called when this GameObject is activated for the first time
+    private void Start()
     {
-        forbiddenLayerIndex = LayerMask.NameToLayer(forbiddenLayerName);
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Collision Detected his layer being " + other.gameObject.layer + " and the forbidden layer being " + forbiddenLayerIndex);
-        if (other.gameObject.layer == forbiddenLayerIndex)
+        _forbiddenLayerIndex = LayerMask.NameToLayer(forbiddenLayerName);
+        // Does a layer with that name exist (-1 means an non-existant layer)
+        if (_forbiddenLayerIndex == -1)
         {
-            Debug.Log("Collision with forbidden Object detected!");
-            if(amountClippedThrough == 0)
-            {
-                Debug.Log("Fading Screen to Black!");
-                animator.SetTrigger("ClippingStarted");
-            }
-            amountClippedThrough++;
+            throw new System.Exception("Unregistered Layer used in ClippingDetector");
         }
     }
 
+    //Is called when a non-Trigger Collider enters this GameObjects Trigger-Collider
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == _forbiddenLayerIndex)
+        {
+            //If the screen isn't black because of this ClippingDetector, fade it to black
+            if(_amountClippedThrough == 0)
+            {
+                animator.SetBool("ClippingObject", true);
+            }
+            _amountClippedThrough++;
+        }
+    }
+
+    //Is called when a non-Trigger Collider leaves this GameObjects Trigger-Collider
     public void OnTriggerExit(Collider other)
     {
-        Debug.Log("Collision ended");
-        if (other.gameObject.layer == forbiddenLayerIndex)
+        if (other.gameObject.layer == _forbiddenLayerIndex)
         {
-            Debug.Log("Collision with forbidden Object ended!");
-            amountClippedThrough--;
-            if (amountClippedThrough == 0)
+            _amountClippedThrough--;
+            // If there are no more Objects with the specified Layer colliding with our GameObjects Collider, fade back in
+            if (_amountClippedThrough == 0)
             {
-                Debug.Log("Fading Screen to White!");
-                animator.SetTrigger("ClippingEnded");
+                animator.SetBool("ClippingObject", false);
             }
         }
     }
