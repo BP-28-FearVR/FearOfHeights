3f804b77e6c18ab4484f8c26006211ed07e62bef
dmc1011 <dmccarroll98@gmail.com>
2024-01-31 13:49:01 +0100

    Kollisionsbehandlung finalisiert

--- Assets/Script/FloorDetector.cs
+++ Assets/Script/FloorDetector.cs
@@ -2,38 +2,37 @@ using UnityEngine;
 
 public class FloorDetector : MonoBehaviour
 {
-    //[SerializeField] private GameObject xrOriginRig;
-    //private Transform _headTransform;
+    [Tooltip("The radius of the Spherecast")]
     [SerializeField] private float radius = 1.5f;
-    private float _maxDistance = 3.0f;
 
-    [Tooltip("The layer which is detected by the sphere cast.")]
+    [Tooltip("The layer which is detected by the Spherecast.")]
     [SerializeField] private LayerMask layerMask;
 
-    [Tooltip("This floor will disappear if the player walks onto it far enough.")]
+    [Tooltip("This object will disappear if the player leaves the layer which is set in layer mask")]
     [SerializeField] private GameObject invisibleFloor;
 
     private RaycastHit _hit;
     private Vector3 _sphereCastDirection = new Vector3(0, -1, 0);
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    private float _maxDistance = 3.0f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (invisibleFloor != null)
         {
-            bool isSpherecastColliding = Physics.SphereCast(transform.position, radius, _sphereCastDirection, out _hit, _maxDistance, layerMask);
+            DisableFloor();
+        }
+    }
 
-            if (!isSpherecastColliding)
-            {
-                invisibleFloor.SetActive(false);
-                this.gameObject.SetActive(false);
-            }
+    // if the player leaves the specified layer, the floor will disappear, causing the player to fall down
+    public void DisableFloor()
+    {
+        bool isSpherecastColliding = Physics.SphereCast(transform.position, radius, _sphereCastDirection, out _hit, _maxDistance, layerMask);
 
-        }  
+        if (!isSpherecastColliding)
+        {
+            invisibleFloor.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
     }
 }
